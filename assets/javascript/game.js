931b4371dedb243b1b6687e4541df5eac8951f84
JessicaSung <jessica.sung@utexas.edu>
2017-01-05 17:31:44 -0600

    Created character buttons; started player choosing logic.

---
+++ assets/javascript/game.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+// VARIABLES
+// ===============================================
+var characters = [
+	{name: 'Obi-Wan Kenobi', img: 'assets/images/obiwan.jpg', hp: 120, ap: 12, ca: 10},
+	{name: 'Luke Skywalker', img: 'assets/images/luke.png', hp: 100, ap: 8, ca: 6},
+	{name: 'Darth Sidious', img: 'assets/images/sideous.jpg', hp: 150, ap: 15, ca: 13},
+	{name: 'Darth Maul', img: 'assets/images/maul.jpg', hp: 180, ap: 6, ca: 4}
+];
+var playerIsChosen = false;
+var enemyIsChosen = false;
+
+// Displaying characters on the page
+for (var i = 0; i < characters.length; i++) {
+	var b = $('<button>');
+	b.addClass('characterButton');
+	b.attr('name', characters[i].name);
+	b.attr('hp', characters[i].hp);
+	b.attr('ap', characters[i].ap);
+	b.attr('ca', characters[i].ca);
+	b.append("<p>" + characters[i].name + "</p><br><img src='" + characters[i].img + "' class='characterImage'><br><p>HP: " + characters[i].hp + "</p>");
+	$('#allCharacters').append(b);
+}
+
+
+// choosing player and opponent
+$(document).on('click', '.characterButton', function() {
+	if (!playerIsChosen) {
+		// save the character name of a button clicked to a variable
+		var playerName = $(this).attr('name');
+		console.log(this);
+		console.log(playerName);
+	}
+	
+	console.log("you clicked a character.");
+});
+
+
+
+
+
+
+
+
+
+
+
+});
